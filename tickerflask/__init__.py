1c9b443a75c68e35ae3fb15b4897e0d64123ad4b
signebedi <signe@siftingwinnowing.com>
2023-06-25 15:16:48 -0500

    Added: fronend routes and secret key

--- tickerflask/__init__.py
+++ tickerflask/__init__.py
@@ -1,9 +1,29 @@
 from .__metadata__ import (__name__, __author__, __credits__, __version__, 
                        __license__, __maintainer__, __email__)
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from .wrappers import get_latest_price, get_price_spread
+import os, uuid
+
+def set_secret_key():
+    # Check if the secret key file exists
+    if os.path.exists('.secret_key'):
+        # If it exists, read the key from the file
+        with open('.secret_key', 'r') as f:
+            secret_key = f.read()
+    else:
+        # If it doesn't exist, generate a new UUID and save it in the file
+        secret_key = str(uuid.uuid4())
+        with open('.secret_key', 'w') as f:
+            f.write(secret_key)
+    return secret_key
 
 app = Flask(__name__)
+app.secret_key = set_secret_key()
+app.static_folder = 'static/'
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html.jinja')
 
 @app.route('/get_latest_price', methods=['GET'])
 def latest_price():
