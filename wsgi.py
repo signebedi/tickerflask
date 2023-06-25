e5bcd6e57d81713c517ecd1908e7c92714bdc9d6
signebedi <signe@siftingwinnowing.com>
2023-06-25 11:51:23 -0500

    Added: wsgi entrypoint

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+import os
+from tickerflask import app
+
+def main():
+    env = os.getenv('FLASK_ENV', 'production')  # If 'FLASK_ENV' isn't set, defaults to 'production'
+    debug = env == 'development'
+    app.run(debug=debug)
+
+if __name__ == "__main__":
+    main()
