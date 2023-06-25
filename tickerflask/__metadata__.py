86c4c109778072d9bfc09407e3777346d65aa464
signebedi <signe@siftingwinnowing.com>
2023-06-25 11:43:52 -0500

    Added: metadata file

---
+++ tickerflask/__metadata__.py
@@ -0,0 +1,7 @@
+__name__ = "tickerflask"
+__author__ = "Sig Janoska-Bedi"
+__credits__ = ["Sig Janoska-Bedi",]
+__version__ = "0.1.0"
+__license__ = "MIT"
+__maintainer__ = "Sig Janoska-Bedi"
+__email__ = "signe@atreeus.com"
