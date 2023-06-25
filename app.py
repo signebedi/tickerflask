fe762a1c7e2448c2ec3108e2d6bfecd873ef01fa
signebedi <signe@siftingwinnowing.com>
2023-06-25 10:52:16 -0500

    Added: modified output to tuple, added docstring

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import yfinance as yf
+from typing import Tuple, Union
 
-def get_latest_price(symbol):
+def get_latest_price(symbol: str, period: str) -> Tuple[str, float]:
+    """
+    Get the datetime and closing price for the last day within the specified period for a given stock symbol.
+    
+    :param symbol: The stock ticker symbol.
+    :param period: The period over which to retrieve data. The acceptable values are '1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'.
+    :return: A tuple where the first element is the datetime of the last available price and the second element is the closing price for that datetime.
+    """
     ticker = yf.Ticker(symbol)
-    todays_data = ticker.history(period='1d')
-    if not todays_data.empty:
-        return todays_data['Close'][0]
+    historical_data = ticker.history(period=period)
+    if not historical_data.empty:
+        last_datetime = str(historical_data.index[-1])  # Get last date (as a string for JSON serializability)
+        last_price = historical_data['Close'][-1]  # Get last available closing price
+        return (last_datetime, last_price)
+
+
 
